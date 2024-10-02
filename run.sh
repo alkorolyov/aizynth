55bfadb06302d317ac1b89873da2fd1a66587926
alkorolyov <korol.al@gmail.com>
2024-10-02 11:11:53 +0200

    update

--- run.sh
+++ run.sh
@@ -1,21 +1,25 @@
-cmd="aizynthcli --smiles data/100_rnd.smi --config config/config.yml --stocks molport_7d --nproc 48"
+cmd="aizynthcli --smiles data/gdb_1000.smi --output results/gdb_1000_az.json.gz --nproc 48 --cluster --config config/config.yml"
 
-# Record the start time
 start_time=$(date +%s)
-
-# Your script or commands go here
 $cmd
-
-# Record the end time
 end_time=$(date +%s)
 
-# Calculate the time difference
 elapsed_time=$((end_time - start_time))
-
-# Convert seconds to hours, minutes, and seconds
 hours=$((elapsed_time / 3600))
 minutes=$(( (elapsed_time % 3600) / 60 ))
 seconds=$((elapsed_time % 60))
 
-# Print the elapsed time
+printf "Elapsed time: %02d:%02d:%02d\n" $hours $minutes $seconds
+
+cmd="aizynthcli --smiles data/chembl_1000.smi --output results/chembl_1000_az.json.gz --nproc 48 --cluster --config config/config.yml"
+
+start_time=$(date +%s)
+$cmd
+end_time=$(date +%s)
+
+elapsed_time=$((end_time - start_time))
+hours=$((elapsed_time / 3600))
+minutes=$(( (elapsed_time % 3600) / 60 ))
+seconds=$((elapsed_time % 60))
+
 printf "Elapsed time: %02d:%02d:%02d\n" $hours $minutes $seconds
