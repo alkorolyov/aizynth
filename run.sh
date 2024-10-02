9d8bc58423854893033154fe6ecf2817312c4916
alkorolyov <korol.al@gmail.com>
2024-10-02 11:29:11 +0200

    update

--- run.sh
+++ run.sh
@@ -1,25 +1,26 @@
-cmd="aizynthcli --smiles data/gdb_1000.smi --output results/gdb_1000_az.json.gz --nproc 48 --cluster --config config/config.yml"
+#!/bin/bash
 
-start_time=$(date +%s)
-$cmd
-end_time=$(date +%s)
+run_aizynthcli() {
+  local smiles_file=$1
+  local output_file=$2
+  local config_file="config/config.yml"
+  local nproc=8
+  
+  cmd="aizynthcli --smiles $smiles_file --output $output_file --config $config_file --nproc $nproc --cluster"
+  
+  start_time=$(date +%s)
+  $cmd
+  end_time=$(date +%s)
 
-elapsed_time=$((end_time - start_time))
-hours=$((elapsed_time / 3600))
-minutes=$(( (elapsed_time % 3600) / 60 ))
-seconds=$((elapsed_time % 60))
+  elapsed_time=$((end_time - start_time))
+  hours=$((elapsed_time / 3600))
+  minutes=$(( (elapsed_time % 3600) / 60 ))
+  seconds=$((elapsed_time % 60))
 
-printf "Elapsed time: %02d:%02d:%02d\n" $hours $minutes $seconds
+  smiles_basename=$(basename "$smiles_file")
 
-cmd="aizynthcli --smiles data/chembl_1000.smi --output results/chembl_1000_az.json.gz --nproc 48 --cluster --config config/config.yml"
+  printf "Elapsed time for %s: %02d:%02d:%02d\n" "$smiles_basename" $hours $minutes $seconds
+}
 
-start_time=$(date +%s)
-$cmd
-end_time=$(date +%s)
-
-elapsed_time=$((end_time - start_time))
-hours=$((elapsed_time / 3600))
-minutes=$(( (elapsed_time % 3600) / 60 ))
-seconds=$((elapsed_time % 60))
-
-printf "Elapsed time: %02d:%02d:%02d\n" $hours $minutes $seconds
+run_aizynthcli "data/gdb_8.smi" "results/gdb_8_az.json.gz"
+run_aizynthcli "data/chembl_8.smi" "results/chembl_8_az.json.gz"
